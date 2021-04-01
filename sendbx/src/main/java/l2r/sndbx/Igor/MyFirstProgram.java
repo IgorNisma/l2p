85307156bff46d4cac5fcdabcc9d844bde103c6d
IgorNisma <komlevigor7@gmail.com>
2021-04-01 14:18:52 +0300

    Проект отвязан в среде разработки от репозитория git

--- sendbx/src/main/java/l2r/sndbx/Igor/MyFirstProgram.java
+++ sendbx/src/main/java/l2r/sndbx/Igor/MyFirstProgram.java
@@ -2,8 +2,9 @@ package l2r.sndbx.Igor;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-	System.out.println("Hello, world!");
-  }
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+    }
 
 }
